f9330c044580460faa75b42f1cfa68ac27ebe2f0
PavelKarshankov <ggaek@list.ru>
2024-03-25 15:39:54 +0300

    HUINIA
    КОМИТ хуйня

--- src/albumComponents/images.js
+++ src/albumComponents/images.js
@@ -2,33 +2,57 @@ const images = [
     {
         id: 1,
         topImage: {
+<<<<<<< Updated upstream
             src: require('./beforeAfter/img/leftSide/i (1).jpg'),
             alt: 'Alt text 1 for top image'
         },
         bottomImage: {
             src: require('./beforeAfter/img/rightSide/i (2).jpg'),
+=======
+            src: require('./beforeAfter/img/leftSide/1.jpg'),
+            alt: 'Alt text 1 for top image'
+        },
+        bottomImage: {
+            src: require('./beforeAfter/img/rightSide/1.jpg'),
+>>>>>>> Stashed changes
             alt: 'Alt text 1 for bottom image'
         }
     },
     {
         id: 2,
         topImage: {
+<<<<<<< Updated upstream
             src: require('./beforeAfter/img/leftSide/бассейн.jpg'),
             alt: 'Alt text 2 for top image'
         },
         bottomImage: {
             src: require('./beforeAfter/img/rightSide/i.jpg'),
+=======
+            src: require('./beforeAfter/img/leftSide/2.jpg'),
+            alt: 'Alt text 2 for top image'
+        },
+        bottomImage: {
+            src: require('./beforeAfter/img/rightSide/2.jpg'),
+>>>>>>> Stashed changes
             alt: 'Alt text 2 for bottom image'
         }
     },
     {
         id: 3,
         topImage: {
+<<<<<<< Updated upstream
             src: require('./beforeAfter/img/leftSide/2017231118244503.jpg'),
             alt: 'Alt text 3 for top image'
         },
         bottomImage: {
             src: require('./beforeAfter/img/rightSide/20172311182503.jpg'),
+=======
+            src: require('./beforeAfter/img/leftSide/3.jpg'),
+            alt: 'Alt text 3 for top image'
+        },
+        bottomImage: {
+            src: require('./beforeAfter/img/rightSide/3.jpg'),
+>>>>>>> Stashed changes
             alt: 'Alt text 3 for bottom image'
         }
     }
